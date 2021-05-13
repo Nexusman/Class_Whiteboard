58b28bafab4bdc0769cf333269c25c7c1e1df677
Gregory Smith <1647online@gmail.com>
2021-05-13 09:10:23 -0400

    -a

--- Practice Folder/script.js
+++ Practice Folder/script.js
@@ -1,8 +1,36 @@
-let testfunction;{
-    testfunction = (length, height, width) => {
-        return length * height * width
-        
-    }
+/*
+Christmas Eve is almost upon us, so naturally we need to prepare some milk and cookies for Santa! Create a function that accepts a Date object and returns true if it's Christmas Eve (December 24th) and false otherwise. Keep in mind JavaScript's Date month is 0 based, meaning December is the 11th month while January is 0.
+
+******  Validate Date With With Date.parse() Method in JavaScript ******
+
+**** RegEx Validation ***
+
+
+
+
+
+function timeForMilkAndCookies(date) {
+    let newDate = Date.parse(date);
+
+
+
+
+	console.log(newDate);
 }
 
-console.log(testfunction(5,5,25));
+
+timeForMilkAndCookies(new Date(2013, 11, 24))
+
+*/
+
+let num = prompt('Enter a Number');
+    if (num > 0){
+        alert('The number is 1');
+    }
+    else if(num < 0){
+        alert('The number is -1');
+    }
+    else{
+        alert('The number is 0');
+    }
+    
